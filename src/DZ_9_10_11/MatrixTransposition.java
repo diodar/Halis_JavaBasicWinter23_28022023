f68bea8657812f830369128f8a7c7d11127aa28e
diodar <mikhailgalis@gmail.com>
2023-03-28 00:08:04 +0300

    DZ_11_upd1

--- src/DZ_9_10_11/MatrixTransposition.java
+++ src/DZ_9_10_11/MatrixTransposition.java
@@ -1,11 +1,13 @@
 package DZ_9_10_11;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class MatrixTransposition {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        Random randomizer = new Random();
 
         System.out.print("Input number of lines 'M': ");
         int m = in.nextInt();
@@ -15,15 +17,17 @@ public class MatrixTransposition {
 
         System.out.println("\n" + "Input matrix M x N: ");
         int[][] inputMatrix = new int[m][n];
+        int[][] transposeMatrix = new int[n][m];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                inputMatrix[i][j] = randomizer.nextInt(10);
+                transposeMatrix[j][i] = inputMatrix[i][j];
                 System.out.print(inputMatrix[i][j] + " ");
             }
             System.out.println();
         }
         System.out.println("\n" + "Transpose matrix N x M: ");
-        int[][] transposeMatrix = new int[n][m];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
